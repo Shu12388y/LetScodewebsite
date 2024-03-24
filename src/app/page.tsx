fd2450410bc1c64208f435901838443a4a0ab539
Shu12388y <spl.sp999@gmail.com>
2024-03-24 10:36:25 +0530

    file addded

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,8 @@ const Card = lazy(() => import("../components/component/TestCard"));
 export default function Home() {
   return (
     <>
-<<<<<<< HEAD
+    <section className="overflow-hidden">
+
       <section className="bg-slate-200 px-10 overflow-hidden">
         <HeroSection />
       </section>
@@ -23,28 +24,8 @@ export default function Home() {
       <section className="bg-green-100 flex flex-1 justify-center">
         <Card />
       </section>
-=======
-    <section className="overflow-hidden">
-
-    <section className="bg-slate-200 px-10 overflow-hidden">
-
-  <HeroSection/>
     </section>
-    <section className="flex flex-1 bg-green-100 justify-center">
-  <Figure/>
-
-    </section>
-    <section className="bg-green-100 flex flex-1 justify-center">
-      <Feature/>
-    </section>
-    <section className="bg-green-100 flex flex-1 justify-center">
-      <Card/>
-    </section>
-    <section className="bg-green-100 flex flex-1 justify-center">
-      <NewsLetter/>
-    </section>
-    </section>
->>>>>>> a238dd5000ecb798b44520a00f9f28e47d5e70e2
+   
     </>
   );
 }
