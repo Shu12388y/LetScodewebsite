39c616b530a77a83981d12a3dc79918c7c00ce95
Shu12388y <spl.sp999@gmail.com>
2024-03-24 10:32:59 +0530

    updated

--- src/components/component/TestCard.tsx
+++ src/components/component/TestCard.tsx
@@ -79,6 +79,81 @@ function TestCard() {
                 </p>
               </div>
             </div>
+            {/* Card 4 */}
+
+            <div className="rounded-2xl p-4 bg-white">
+              <div className="text-center">
+                <div className="flex justify-center items-center gap-2">
+                  <Image
+                    width={500}
+                    height={500}
+                    alt="testimonial"
+                    className="w-20 h-20 mb-4 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
+                    src="https://media.licdn.com/dms/image/D4D35AQFk-cjqD2InDg/profile-framedphoto-shrink_400_400/0/1694364186357?e=1711861200&v=beta&t=-LKkz7BBa6p55UEWS0n0FwQhL17kJHEM3yhghed62ws"
+                  />
+                  <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">
+                    Amishika Batra
+                  </h2>
+                </div>
+                <p className="leading-relaxed">
+                  Your overall material is very good. But can you also provide
+                  data science interview questions like what GoComet and Miq
+                  companies ask for this role. Just to get an idea.
+                </p>
+              </div>
+            </div>
+
+            {/* card 5 */}
+
+            <div className="rounded-2xl p-4 bg-white">
+              <div className="text-center">
+                <div className="flex justify-center items-center gap-2">
+                  <Image
+                    width={500}
+                    height={500}
+                    alt="testimonial"
+                    className="w-20 h-20 mb-4 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
+                    src="https://media.licdn.com/dms/image/D4D35AQGTQrOXVPMjfA/profile-framedphoto-shrink_400_400/0/1710442330370?e=1711861200&v=beta&t=K2W-PQ2KvJEjDmYEjdLZ_MR15FQvCcIMP6TsizhGwWE"
+                  />
+                  <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">
+                    Megha Chhapre
+                  </h2>
+                </div>
+                <p className="leading-relaxed">
+                  I wanted to extend a heartfelt thankyou for the valuable
+                  information you share with us students. For being a source of
+                  guidance and support for all of us, Your generosity in sharing
+                  your knowledge is commendable, and we feel fortunate to
+                  benefit from your expertise.
+                </p>
+              </div>
+            </div>
+
+            {/* card 6 */}
+            <div className="rounded-2xl p-4 bg-white">
+              <div className="text-center">
+                <div className="flex justify-center items-center gap-2">
+                  <Image
+                    width={500}
+                    height={500}
+                    alt="testimonial"
+                    className="w-20 h-20 mb-4 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100"
+                    src="https://media.licdn.com/dms/image/D5603AQElCdChPX2JZA/profile-displayphoto-shrink_400_400/0/1704464004528?e=1717027200&v=beta&t=VZH8nG62jHz1yN4SkO8r8WaTpfZftIC35sapH-Ai30s"
+                  />
+                  <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">
+                    Prachi Kesharwani
+                  </h2>
+                </div>
+                <p className="leading-relaxed">
+                  I want to express my deepest gratitude to each and every
+                  member of this Let's Code 🤗 community (Avinash sir
+                  especially). Your support and dedication have played a
+                  significant role in helping me access invaluable technical
+                  resources and fostering my growth in the computer science
+                  domain. 
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </section>
